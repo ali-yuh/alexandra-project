3f82c3c814bc254a7c24d6675fa73c5fe698cf33
aliyah <aliyahjinw@gmail.com>
2024-12-04 05:14:29 +1000

    almost done

--- script/script.js
+++ script/script.js
@@ -19,7 +19,26 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-
+// dropdown menu
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 
 
 
@@ -41,5 +60,15 @@ document.addEventListener("DOMContentLoaded", () => {
     closeButton.addEventListener("click", closePopup);
 
     // simulate popup opening (for demo purposes)
-    setTimeout(showPopup, 2000);
+    setTimeout(showPopup, 1000);
+});
+
+const giftBox = document.getElementById('giftBox');
+const textBox = document.getElementById('textBox');
+
+giftBox.addEventListener('click', () => {
+  giftBox.classList.add('hidden');
+  setTimeout(() => {
+    textBox.classList.add('visible');
+  }, 500);
 });
